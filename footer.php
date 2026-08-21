e3a44efc88748fcb67611ac1aae0f577edc8a5fe
Gustavo870 <20241TBOR0020012@estudantes.ifpr.edu.br>
2026-08-21 02:56:16 +0000

    a

--- footer.php
+++ footer.php
@@ -8,70 +8,24 @@
 
             <div class="footer-coluna">
 
-                <h3>
-                    NAVEGAÇÃO
-                </h3>
-
-                <a href="index.php">
-                    INÍCIO
-                </a>
-
-                <a href="#sinais">
-                    SINAIS NA TI (DICIONÁRIO)
-                </a>
-
-                <a href="#libras">
-                    IMPORTÂNCIA DA LIBRAS
-                </a>
-
-                <a href="#equidade">
-                    EQUIDADE NA EDUCAÇÃO
-                </a>
-
-                <a href="#">
-                    CONTATO
-                </a>
-
+                <h3>NAVEGAÇÃO</h3>
+                <a href="index.php">INÍCIO</a>
+                <a href="#sinais">SINAIS NA TI (DICIONÁRIO)</a>
+                <a href="#libras">IMPORTÂNCIA DA LIBRAS</a>
+                <a href="#equidade">EQUIDADE NA EDUCAÇÃO</a>
+                <a href="#">CONTATO</a>
             </div>
-
-
-
-            <div class="footer-coluna">
-
-                <h3>
-                    CONTATO
-                </h3>
-
-                <p>
-                    TECHLIBRAS@GMAIL.COM
-                </p>
-
-            </div>
-
-
         </div>
-
-
         <div class="copyright">
-
             © 2025 PLATAFORMA DE INCLUSÃO EM INFORMÁTICA.
             TODOS OS DIREITOS RESERVADOS.
-
         </div>
-
-
         <p class="footer-frase">
             Conectando Libras, tecnologia e educação.
         </p>
-
-
     </div>
-
 </footer>
 
-
-
-<!-- Bootstrap JS -->
 <script
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js">
 </script>
